c1cf0c08964c92d8a679c669effc2d0cdfcc3222
haiyen2304 <hhyhoanhaiyen2304@gmail.com>
2024-07-11 21:43:42 +0700

    em nop bài a

---
+++ session10-making.js
@@ -0,0 +1,56 @@
+// bài 1 kiểm tra a chia hết cho b
+// let a=+prompt("nhập số a vào")
+// let b=+prompt("nhập số b vào")
+// if( a%b===0 ){
+//     alert("a chia hết cho b")
+// };
+
+// bài 2 nhập tuổi đủ đ k vào 10 
+// let tuoi=+prompt("hãy nhập số tuổi của bạn")
+// if(Number.isInteger(tuoi)<=15){
+//     alert("không đủ tuổi vào học")
+// }
+// else{alert("đủ tuổi vào học")}
+
+// bài 3 Nhập một số nguyên bất kỳ và in kết quả ra màn hình
+// để nói cho người dùng biết số đó là lớn hay nhỏ hơn 0
+
+// let a=+prompt("hãy nhập 1 số nguyên bất kì")
+// if(Number.isInteger(a) <0){
+//    alert("số nguyên nhỏ hơn 0");
+// }else if(Number.isInteger(a) >0){
+//    alert("số nguyên lớn hơn 0");
+   
+// }else {alert("số bạn nhập là số 0")}
+
+// // bài 4
+    // let a= +prompt("nhập số nguyên thứ nhất");
+    // let b= +prompt("nhập số nguyên thứ2");
+    // let c= +prompt("nhập số nguyên thứ3");
+    
+    // if(a % 1 !== 0 ||b % 1 !== 0 ||b % 1 !== 0){
+    //      alert("vui lòng nhập lại")
+    // }
+    
+    // if(a>b && b>c){
+    //     alert("a lớn nhất");
+    // }else if (b>a && b>c){
+    //     alert("b lớn nhất");
+    // }else if ( c>a && a>b){
+    //     alert("c lớn nhất")
+    // }
+
+
+    //bài 5
+//  let a =  +prompt("điền điểm giữa kì");
+//  let b =  +prompt("điền điểm cuối kì");
+//  let c= (a+b)/2
+//    if(c>=0 && c<5){
+//       alert("điểm dưới trung bình")
+//    }else if(c<=6){
+//     alert("điểm trung bình")
+//    }else if(c<8){
+//     alert("điểm khá")
+//    }else if(c<10){
+//     alert("điểm giỏi")
+//    }else{("vui lòng điền lại")}
